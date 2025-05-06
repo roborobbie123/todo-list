53d0c5cfb5e6b6a324c99c491bc46f883e1b501e
Robert Barrett <roborobbie123@gmail.com>
2025-05-06 11:08:48 -0400

    added edit feature

--- main.py
+++ main.py
@@ -8,39 +8,50 @@ while True:
             todo = input("Enter a todo: ")
             todo_list.append(todo)
             print(f"Added {todo.upper()} to todo list")
+
         case 'show' | 'display':
-            print("TODO List:")
-            for i in range(len(todo_list)):
-                print(f"{i + 1}) {todo_list[i].title()}")
+            if len(todo_list) == 0:
+                print("Todo list is empty")
+                continue
+            else:
+                print("TODO List:")
+                for i in range(len(todo_list)):
+                    print(f"{i + 1}) {todo_list[i].title()}")
+
         case 'edit':
             if len(todo_list) == 0:
                 print("Todo list is empty")
                 continue
             for i in range(len(todo_list)):
+                print("TODO List:")
                 print(f"{i + 1}) {todo_list[i].title()}")
-            index = int(input("Enter an item number to delete: "))
+            index = int(input("Enter an item number to edit: "))
             if index > len(todo_list):
                 print("Invalid item")
                 continue
             edit = input("Enter updated value: ")
-            todo_list[index - 1].replace(edit)
+            todo_list[index - 1] = edit
+            for i in range(len(todo_list)):
+                print("TODO List:")
+                print(f"{i + 1}) {todo_list[i].title()}")
 
         case 'delete':
             if len(todo_list) == 0:
                 print("Todo list is empty")
                 continue
             for i in range(len(todo_list)):
+                print("TODO List:")
                 print(f"{i + 1}) {todo_list[i].title()}")
-            index = int(input("Enter an item number to edit: "))
+            index = int(input("Enter an item number to delete: "))
             if index > len(todo_list):
                 print("Invalid item")
                 continue
-
             print(f"Deleting {todo_list[index - 1]}...")
             todo_list.remove(todo_list[index - 1])
 
         case 'exit' | 'leave':
             print("Exiting...")
             break
+
         case default:
             print("Please enter a valid input")
